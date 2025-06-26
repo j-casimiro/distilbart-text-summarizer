96f1e06183eceef4e99eefbbefe57ee56adc76f5
Jehu-RND <jehubaclecasimiro@gmail.com>
2025-06-26 11:44:59 +0800

    implement register and login endpoint

--- main.py
+++ main.py
@@ -55,3 +55,51 @@ def index():
         'version': '0.0.1',
         'contributor/s': ['Jehu Casimiro']
     }
+
+
+# register user
+@app.post('/register', response_model=UserRead)
+def register(user: UserCreate, session: Session = Depends(get_session)):
+    user_exists = session.exec(select(User).where(User.email == user.email)).first()
+    if user_exists:
+        raise HTTPException(status_code=400, detail='email already exist')
+    
+    if not verify_email(user.email):
+        raise HTTPException(status_code=400, detail='email is invalid')
+    
+    db_user = User(name=user.name, email=user.email, hashed_password=get_password_hash(user.password))
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return {'id': db_user.id, 'email': db_user.email}
+
+
+@app.post('/login')
+def login(
+    response: Response,
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session)
+):
+    user = session.exec(select(User).where(User.email == form_data.username)).first()
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail='Invalid Credentials')
+    
+    access_token = create_access_token(data={'sub': str(user.id)})
+    refresh_token = create_refresh_token(data={'sub': str(user.id)})
+
+    # set refresh token in HTTP-only cookie
+
+    response.set_cookie(
+        key="refresh_token",
+        value=refresh_token,
+        httponly=True,
+        samesite='strict',
+        max_age=60 * 60 * 24 * 7 # 7 days
+    )
+
+    return {
+        'access_token': access_token,
+        'refresh_token': refresh_token,
+        'user': user,
+        'token_type': 'bearer'
+    }
